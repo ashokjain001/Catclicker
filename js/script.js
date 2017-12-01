257de7c597053b2b2b066685d0e57eb7d4c21325
ashok <ashokjain001@gmail.com>
2017-11-30 18:44:17 -0600

    updating index.html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-
-	var number = 0;
+var number = 0;
         $( ".pic" ).click(function() {
             number = number + 1;
             $(".number").text(number);
+			$("#audio")[0].play();
+	
+        });
+		
+	    $( ".reset" ).click(function() {
+			 number = 0;
+			$(".number").text(number);
         });
